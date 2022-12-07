a5586e8aa775e414313b078350805408e78d6e1b
lian-c <lian.chan@telus.com>
2022-12-06 23:41:31 -0800

    Update code to have the counter drop

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,12 +3,10 @@ $(document).ready(function() {
   $('textarea').on('input', function(event) {
    
     console.log(this.value)
-    console.log(this.value.length)
-    console.log(this.parentNode.lastElementChild)
-    const counter = this.parentNode.lastElementChild.childNodes[3].innerHTML;
-    console.log(counter);
-
+    let textLength = (this.value.length)
+   let counter = this.parentNode.lastElementChild.childNodes[3]
+    counter.innerHTML  =  (140-textLength);
+    
   });
-
 
 });
