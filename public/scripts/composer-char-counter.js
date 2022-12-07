9f3c77d1fcff93b7b56273b9ac2766ea4b5fd386
lian-c <lian.chan@telus.com>
2022-12-06 23:49:12 -0800

    add if statements to make counter red (and remove red) based on text length

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,11 +2,16 @@ $(document).ready(function() {
 
   $('textarea').on('input', function(event) {
    
-    console.log(this.value)
-    let textLength = (this.value.length)
-   let counter = this.parentNode.lastElementChild.childNodes[3]
-    counter.innerHTML  =  (140-textLength);
+    let textLength = (this.value.length);
+   let counter = this.parentNode.lastElementChild.childNodes[3];
+    counter.innerHTML  =  140 - textLength;
     
+    if (textLength > 140) {
+      $(counter).addClass("overlimit")
+    } 
+    if (textLength <= 140) {
+      $(counter).removeClass("overlimit")
+    }
   });
 
 });
