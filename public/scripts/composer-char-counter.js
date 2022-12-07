fc31681c1115737bc767f57e445fc73c3461ebca
lian-c <lian.chan@telus.com>
2022-12-06 22:26:25 -0800

    Create variable holding the counter by traverse through this

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,8 +2,11 @@ $(document).ready(function() {
 
   $('textarea').on('input', function(event) {
    
+    console.log(this.value)
     console.log(this.value.length)
-
+    console.log(this.parentNode.lastElementChild)
+    const counter = this.parentNode.lastElementChild.childNodes[3].innerHTML;
+    console.log(counter);
 
   });
 
