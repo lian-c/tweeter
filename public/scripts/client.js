6e9c736a4619c24a5d731a19c3968d1dd76e38f5
lian-c <lian.chan@telus.com>
2022-12-07 17:28:16 -0800

    Update client.js ensure renderTweets function is working for JSON and shown on page

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,22 +7,39 @@
 
 // Test / driver code (temporary). Eventually will get this from the server.
 $(() => {
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
+
+  // Fake data taken from initial-tweets.json
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
     },
-  "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-  "created_at": 1461116232227
-}
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
+
 
 const renderTweets = function(tweets) {
   for (const tweet of tweets){
     const $tweet = createTweetElement(tweet);
-    $('current-tweets').prepend($tweet);
+    $('.current-tweets').prepend($tweet);
+    console.log($tweet)
   }
 }
 
@@ -48,12 +65,9 @@ const createTweetElement = function(tweetObject){
         </footer>
       </article>
   `);
-  return $tweetHTML
+return $tweetHTML
 }
-const $tweet = createTweetElement(tweetData);
-// Test / driver code (temporary)
-console.log(tweetData.user.name)
-console.log($tweet); // to see what it looks like
-$('.current-tweets').prepend($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
 
+
+renderTweets(data)
 });
